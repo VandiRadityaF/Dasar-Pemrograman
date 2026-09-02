5e44fb1322980ef32a548d1ff5dbd365aa2150a9
VandiRadityaF <vandiradityafirmansyah@gmail.com>
2026-09-02 22:27:29 +0700

    Add files via upload

---
+++ Dasar Pemrograman/Project/jobsheet01/Tugas29.java
@@ -0,0 +1,9 @@
+public class Tugas29 {
+    public static void main(String[] args) {
+
+        System.out.println("Setelah Langkah 1: Nampan A = Kosong, Nampan B = Bola Bulan, Nampan C = Bola Bintang");
+        System.out.println("Setelah Langkah 2: Nampan A = Bola Bulan, Nampan B = Kosong, Nampan C = Bola Bintang");
+        System.out.println("Setelah Langkah 3: Nampan A = Bola Bulan, Nampan B = Bola Bintang, Nampan C = Kosong");
+        System.out.println("Kesimpulan: Pernyataan yang benar adalah (a) dan (e)");
+    }
+}
