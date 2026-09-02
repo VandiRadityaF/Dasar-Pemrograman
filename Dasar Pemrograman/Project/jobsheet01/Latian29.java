4183ec2d11786dd9c8ffca8c135042d0440f68a6
VandiRadityaF <vandiradityafirmansyah@gmail.com>
2026-09-02 22:26:50 +0700

    Add files via upload

---
+++ Dasar Pemrograman/Project/jobsheet01/Latian29.java
@@ -0,0 +1,9 @@
+public class Latian29 {
+    
+    public static void main(String[] args) {
+        System.out.println("Halo, nama saya adalah Vandi Raditya Firmansyah");
+
+System.out.println("Belajar Java itu mudah!");
+System.out.println("Semakin sering mencoba, semakin jago.");
+    }
+}
