2ad4a37ff45a28b340ed2328cc052992e1be5f3a
VandiRadityaF <vandiradityafirmansyah@gmail.com>
2026-09-04 16:38:26 +0700

    Add files via upload

---
+++ Dasar Pemrograman/Tugas Teori/Pertemuan02/Tugast01_29.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Tugast01_29 {
+    public static void main(String[] args) {
+
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print ("Masukan gaji pokok:");
+            int gajipokok = scan.nextInt();
+
+            System.out.print ("Masukan tunjangan anak");
+            int tunjangananak = scan.nextInt();
+
+            System.out.print("Masukan jumlah anak");
+            int jumlahanak = scan.nextInt();
+
+            double potongan = 0.10;
+
+            double gajibersih = (gajipokok - (gajipokok * potongan)) + (tunjangananak * jumlahanak);
+            System.out.println("Gaji bersih" + gajibersih);
+        }
+    }
+}
